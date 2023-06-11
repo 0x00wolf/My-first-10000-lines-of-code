a38fcdd89cab8baace1394b76e4346dcfba74398
Wolf <104569969+0x00wolf@users.noreply.github.com>
2023-06-11 12:53:51 -0700

    Delete CS50x/week3 directory

--- CS50x/week3/lab3/calc.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    float a = get_float("usr time: ");
-    float b = get_float("sys time: ");
-    printf("Running time: %f\n", (a + b));
-}

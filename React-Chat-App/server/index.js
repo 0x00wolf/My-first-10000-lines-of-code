20766e0405459a7feb125f60621807e1b63d6050
Wolf <104569969+0x00wolf@users.noreply.github.com>
2023-06-16 13:36:25 -0700

    Add files via upload

---
+++ React-Chat-App/server/index.js
@@ -0,0 +1,48 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+let users = []
+
+const socketIO = require('socket.io')(http, {
+	cors: {
+		origin: "http://localhost:3000"
+	}
+});
+
+socketIO.on('connection', (socket) => {
+	console.log(` : ${socket.id} user just connected!`);
+
+	socket.on('message', (data) => {
+		socketIO.emit('messageResponse', data);
+	});
+
+	socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
+	socket.on('newUser', (data) => {
+		users.push(data);
+		socketIO.emit('newUserResponse', users);
+	});
+
+	socket.on('disconnect', () => {
+		console.log(" : A user disconnected"); 
+		users = users.filter(user => user.socketID !== socket.id);
+		socketIO.emit('newUserResponse', users);
+		socket.disconnect();
+	});
+});
+
+app.get('/api', (req, res) => {
+	res.json({
+		message: 'Hello World!',
+	});
+});
+
+http.listen(PORT, () => {
+	console.log(`Server listening on ${PORT}`);
+});
